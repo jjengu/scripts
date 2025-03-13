a67fcb55f0ce099d5340006161f56a9a22192a06
jengu <124980843+jjengu@users.noreply.github.com>
2025-03-13 10:32:07 -0400

    Update loader.lua

--- private/loader.lua
+++ private/loader.lua
@@ -1,7 +1,7 @@
 local Scripts = {
-    [6361937392] = "https://raw.githubusercontent.com/jjengu/scripts/refs/heads/main/private/stfobtb/script.lua",
-    [6356763358] = "https://raw.githubusercontent.com/jjengu/scripts/refs/heads/main/private/stfobtb/script.lua",
-    [11515893037] = "https://raw.githubusercontent.com/jjengu/scripts/refs/heads/main/private/stfobtb/script.lua"
+    [6361937392] = "https://raw.githubusercontent.com/jjengu/scripts/refs/heads/main/private/stfoabtb/script.lua",
+    [6356763358] = "https://raw.githubusercontent.com/jjengu/scripts/refs/heads/main/private/stfoabtb/script.lua",
+    [11515893037] = "https://raw.githubusercontent.com/jjengu/scripts/refs/heads/main/private/stfoabtb/script.lua"
 }
 local URL = Scripts[game.PlaceId]
 
